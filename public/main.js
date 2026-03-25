e1a44ae8376334a43b69c81a48f91ad8e4e7c36b
Jorge Auquilla <jorge.auquilla99@myhunter.cuny.edu>
2026-03-25 11:50:35 -0400

    incorporated split text animation for the album info section as well as including a perspective tilt for the album cover when moving around the mouse

--- public/main.js
+++ public/main.js
@@ -4,6 +4,10 @@ import {
   press,
 } from "https://cdn.jsdelivr.net/npm/motion@12.38.0/+esm";
 
+gsap.registerPlugin(ScrambleTextPlugin, SplitText);
+
+/* Using Motion.Js to add a hover & press effect on the nav bar */
+
 hover(".album_c", (element) => {
   animate(element, { scale: 0.9 });
   return () => animate(element, { scale: 1 });
@@ -15,7 +19,63 @@ press(".album_c", (element) => {
   return () =>
     animate(element, { scale: 1 }, { type: "spring", stiffness: 500 });
 });
-document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrambleTextPlugin, SplitText);
-  // gsap code here!
+
+/* Cursor Driven Perspective when moving around the album cover in the album page */
+
+const main = document.querySelector("main");
+
+gsap.set("main", { perspective: 650 });
+
+const outerRX = gsap.quickTo(".a-c", "rotationX", { ease: "power3" });
+const outerRY = gsap.quickTo(".a-c", "rotationY", { ease: "power3" });
+const innerX = gsap.quickTo(".a", "x", { ease: "power3" });
+const innerY = gsap.quickTo(".a", "y", { ease: "power3" });
+
+main.addEventListener("pointermove", (e) => {
+  outerRX(gsap.utils.interpolate(15, -15, e.y / window.innerHeight));
+  outerRY(gsap.utils.interpolate(-15, 15, e.x / window.innerWidth));
+  innerX(gsap.utils.interpolate(-30, 30, e.x / window.innerWidth));
+  innerY(gsap.utils.interpolate(-30, 30, e.y / window.innerHeight));
 });
+
+main.addEventListener("pointerleave", (e) => {
+  outerRX(0);
+  outerRY(0);
+  innerX(0);
+  innerY(0);
+});
+
+/* Split Text Library Functions for the album info in the album page */
+
+let split, animation;
+
+function splitTextChars() {
+  split = SplitText.create(".album-text", { type: "chars,words,lines" });
+  return split;
+}
+splitTextChars();
+gsap.from(split.chars, {
+  x: 150,
+  opacity: 0,
+  duration: 1.5,
+  ease: "power4",
+  stagger: 0.02,
+});
+
+// gsap.from(split.lines, {
+//   rotationX: -100,
+//   transformOrigin: "50% 50% -160px",
+//   opacity: 0,
+//   duration: 2.5,
+//   ease: "power3",
+//   stagger: 1.7,
+// });
+
+// gsap.from(split.words, {
+//   y: -100,
+//   opacity: 0,
+//   rotation: "random(-80, 80)",
+//   duration: 0.5,
+//   ease: "back",
+//   stagger: 0.15,
+// });
